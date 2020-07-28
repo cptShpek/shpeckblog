0421d7ca6e10ce484e5d60aaf5ef4b0b08b9ab08
cptShpek <tarzansh1397@gmail.com>
2020-07-28 18:44:47 +0300

    build commit

---
+++ precache-manifest.64f1fb06f1d62ff638ba688230ac841f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89d17ec3fea33f1d0a555c1b69ac638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a9ad6001283b6fe5fe2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "26a848c313bd37fc1e98",
+    "url": "/static/js/2.98435c20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.98435c20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a9ad6001283b6fe5fe2",
+    "url": "/static/js/main.6666712a.chunk.js"
+  },
+  {
+    "revision": "5ecad0009b878d504a61",
+    "url": "/static/js/runtime-main.1199e4c2.js"
+  }
+]);
